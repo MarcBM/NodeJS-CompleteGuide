d94a26bb5b88749362e5ae5e9656d500f6343e48
MarcBM <marc.betbeder@hotmail.com>
2021-03-07 12:05:01 +1100

    Finished Module

--- DynamicTemplates/routes/admin.js
+++ DynamicTemplates/routes/admin.js
@@ -2,18 +2,32 @@
 const path = require('path');
 // Node-Specific Modules
 const express = require('express');
-
+// Custom Utility Modules
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
+// We may want to store the data we recieve from the user. Let's do that here in an array.
+// The way that we are storing this data currently is not ideal, it's actually really risky and unsafe.
+// The data is mutable across all requests, including from various users at once, which is a big headache.
+// This is the way we will do it for now, but is definitely not how it should be done in future.
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', {
+        docTitle: "Add Product",
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    })
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    // Here we save the product to our array.
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;

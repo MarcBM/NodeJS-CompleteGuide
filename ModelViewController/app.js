9e6e4546e29bc4876eda7cb8e9e2c13297f31e10
MarcBM <marc.betbeder@hotmail.com>
2021-11-05 14:30:18 +1100

    Lesson 99 - Finishing the Controllers
    Added a controller for the 404 page in a new controller file.

--- ModelViewController/app.js
+++ ModelViewController/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 // Custom Routes Modules
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+// Custom Controller Modules
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -19,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
---
+++ ModelViewController/controllers/error.js
@@ -0,0 +1,6 @@
+// This is a controller that will handle errors.
+
+// Get 404 - Used in app.js
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', {docTitle: 'Page not Found'});
+};

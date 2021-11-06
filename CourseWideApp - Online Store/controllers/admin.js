50d9e795edd2466d6b858417a44b6d309cf6e309
MarcBM <marc.betbeder@hotmail.com>
2021-11-06 18:26:01 +1100

    Lesson 110 - Storing Product Data
    Added logic to retrieve more data when adding a product.

--- CourseWideApp - Online Store/controllers/admin.js
+++ CourseWideApp - Online Store/controllers/admin.js
@@ -13,8 +13,12 @@ exports.getAddProduct = (req, res, next) => {
 
 // Post AddProduct - Used in routes/admin.js.
 exports.postAddProduct = (req, res, next) => {
-    // Here we save the product to our array.
-    const product = new Product(req.body.title);
+    // Here we save the product to our file.
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };

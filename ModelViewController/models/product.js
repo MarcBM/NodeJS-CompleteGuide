a37e401e843c58b5baa9a94eb9e71c56d9df175e
MarcBM <marc.betbeder@hotmail.com>
2021-11-06 16:13:04 +1100

    Lesson 101 - Storing Data in Files via the Model
    Added a bunch of jankiness that is unnecessary since we are going to have a database eventually. Might cop it and use this for the Timers though.

--- ModelViewController/models/product.js
+++ ModelViewController/models/product.js
@@ -1,7 +1,13 @@
 // This file will model a single 'product' entity.
 // This is where our OO Programming works.
 
-const products = [];
+// We used to use an array to store all of our products, but it is much better to use a file instead.
+// Eventually, we will implement a proper database to house all of the data, but for now a file will do.
+// const products = [];
+
+// Core Node Modules.
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(title) {
@@ -9,10 +15,34 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll() {
-        return products;
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return [];
+            }
+            return JSON.parse(fileContent);
+        })
     }
 }

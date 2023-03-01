dd48de2178987004ab332517a9864f38f1282df7
Vuldyn <marc.betbeder@hotmail.com>
2023-03-01 18:09:50 +1100

    Lesson 144
    Loading products from the db using SQL

--- CourseWideApp - Online Store/controllers/shop.js
+++ CourseWideApp - Online Store/controllers/shop.js
@@ -7,13 +7,15 @@ const Cart = require('../models/cart');
 
 // Get Products data - Used in routes/shop.js.
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll(products => {
-        res.render('shop/product-list', {
-            prods: products,
-            pageTitle: 'All Products',
-            path: '/products'
-        });
-    });
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/product-list', {
+                prods: rows,
+                pageTitle: 'All Products',
+                path: '/products'
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
@@ -28,13 +30,15 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    const products = Product.fetchAll(products => {
-        res.render('shop/index', {
-            prods: products,
-            pageTitle: 'Shop',
-            path: '/'
-        });
-    });
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/index', {
+                prods: rows,
+                pageTitle: 'Shop',
+                path: '/'
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {

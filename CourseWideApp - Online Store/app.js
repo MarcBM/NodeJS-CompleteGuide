c66e42f076938b60238e9873301b32111cbf4335
Vuldyn <marc.betbeder@hotmail.com>
2023-03-03 19:03:17 +1100

    Lesson 153
    Added command to sync JS definitions with MySQL DB as tables.

--- CourseWideApp - Online Store/app.js
+++ CourseWideApp - Online Store/app.js
@@ -9,12 +9,11 @@ const shopRoutes = require('./routes/shop');
 // Custom Controller Modules
 const errorController = require('./controllers/error');
 // Custom Database Module
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs')
-
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,5 +23,14 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+// Command to sync all defined models in the app with the MySQL database.
+// Sequelize creates/updates any new/changed definitions into tables.
+sequelize.sync().then(result => {
+    // console.log(result)
+})
+.catch(err => {
+    console.log(err);
+});
 
 app.listen(3000);

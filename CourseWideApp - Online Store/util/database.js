868e019758a1a980b51c10ea1ff18000fb53fc8e
Marc Betbeder-Matibet <marc.betbeder@hotmail.com>
2023-10-01 17:30:12 +1100

    180 - Initial MongoDB Import

--- CourseWideApp - Online Store/util/database.js
+++ CourseWideApp - Online Store/util/database.js
@@ -1,9 +1,20 @@
-// Capital S for Sequelize to show that we are importing Sequelize as a class.
-const Sequelize = require('sequelize');
-// Lowercase s for sequelize as we are instantiating a new sequelize object.
-const sequelize = new Sequelize('node-complete', 'root', '-K@WK3YJFD6o*Y2_Ue6y', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-// Remember to export the sequelize object, so that we can use it in app.js.
-module.exports = sequelize;
+// Here we import the mongodb library
+const mongodb = require('mongodb');
+
+// From the mongodb library, we create an instance of the MongoClient.
+const MongoClient = mongodb.MongoClient;
+
+// So we can expose our connection logic to app.js, we are wrapping it in an arrow function, which takes a callback that will be able to use the resulting client, allowing us to access the database.
+const mongoConnect = callback => {
+    // To connect to the database, all we need to do is use the connect method. This method returns a promise, which we are familiar with at this point.
+    MongoClient.connect(
+        'mongodb+srv://marcbetbeder:k5QfYL8s8UnKlZvs@cluster0.9h8qw14.mongodb.net/?retryWrites=true&w=majority'
+    )
+        .then(client => {
+            console.log('Connected to MongoDB');
+            callback(client);
+        })
+        .catch(err => console.error(err));
+}
+
+module.exports = mongoConnect;

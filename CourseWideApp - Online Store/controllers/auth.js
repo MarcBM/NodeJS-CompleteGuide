e77ee8fb2ef48825639b6c59e2c345a951fc5007
Marc Betbeder-Matibet <marc.betbeder@hotmail.com>
2024-02-13 16:36:48 +1100

    257 - Adding LogIn functionality
    Now when logging in we actually test the email/password combination before creating a session and logging in.

--- CourseWideApp - Online Store/controllers/auth.js
+++ CourseWideApp - Online Store/controllers/auth.js
@@ -18,15 +18,36 @@ exports.getSignup = (req, res, next) => {
 	});
 };
 
+// Instead of just finding our dummy user and creating a session, we are going to find the user based on their provided information.
 exports.postLogin = (req, res, next) => {
-	User.findById('5bab316ce0a7c75f783cb8a8')
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({ email: email })
 		.then(user => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save(err => {
-				console.log(err);
-				res.redirect('/');
-			});
+			if (!user) {
+				// If no such user exists based on the email provided, we return to the login page. We should post an error to the user here telling them why they didn't manage to log in.
+				return res.redirect('/login');
+			}
+			// If we made it passed here, we know the email exists, but we still need to check the password.
+			// We can use bcrypt to re-hash the password and compare it to the hashed password in the database.
+			bcrypt
+				.compare(password, user.password)
+				// bcrypt.compare will not throw an error if the passwords do not match. Instead, it will return a boolean value. We can use this to determine if the passwords match or not.
+				.then(doMatch => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save(err => {
+							console.log(err);
+							res.redirect('/');
+						});
+					}
+					res.redirect('/login');
+				})
+				.catch(err => {
+					console.log(err);
+					res.redirect('/login');
+				});
 		})
 		.catch(err => console.log(err));
 };
@@ -42,21 +63,22 @@ exports.postSignup = (req, res, next) => {
 	User.findOne({ email: email })
 		.then(userDoc => {
 			if (userDoc) {
-				// If the user exists, we are redirecting back to the sign-up page.
+				// If the user exists, we are redirecting back to the sign-up page. We should post an error to the user here telling them why they didn't manage to sign-up.
 				return res.redirect('/signup');
 			}
-			return bcrypt.hash(password, 12);
-		})
-		.then(hashedPassword => {
-			const user = new User({
-				email: email,
-				password: hashedPassword,
-				cart: { items: [] }
-			});
-			return user.save();
-		})
-		.then(result => {
-			res.redirect('/login');
+			return bcrypt
+				.hash(password, 12)
+				.then(hashedPassword => {
+					const user = new User({
+						email: email,
+						password: hashedPassword,
+						cart: { items: [] }
+					});
+					return user.save();
+				})
+				.then(result => {
+					res.redirect('/login');
+				});
 		})
 		.catch(err => {
 			console.log('User Sign-Up Error: ' + err);

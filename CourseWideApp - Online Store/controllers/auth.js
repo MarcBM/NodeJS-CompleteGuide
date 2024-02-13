d65d4482707ebf9c064329ed57d9834308b788cd
Marc Betbeder-Matibet <marc.betbeder@hotmail.com>
2024-02-13 16:47:41 +1100

    259 - Using Middleware to protect routes
    Added a middleware function that can easily be used to protect any routes that require login.

--- CourseWideApp - Online Store/controllers/auth.js
+++ CourseWideApp - Online Store/controllers/auth.js
@@ -38,7 +38,9 @@ exports.postLogin = (req, res, next) => {
 						req.session.isLoggedIn = true;
 						req.session.user = user;
 						return req.session.save(err => {
-							console.log(err);
+							if (err) {
+								console.log('Login Error: ' + err);
+							}
 							res.redirect('/');
 						});
 					}
@@ -87,7 +89,9 @@ exports.postSignup = (req, res, next) => {
 
 exports.postLogout = (req, res, next) => {
 	req.session.destroy(err => {
-		console.log(err);
+		if (err) {
+			console.log('Logout Error: ' + err);
+		}
 		res.redirect('/');
 	});
 };

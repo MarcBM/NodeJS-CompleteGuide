2c1af8b799bbf7896efb3374f3909b38af9aa227
Marc Betbeder-Matibet <marc.betbeder@hotmail.com>
2024-02-14 13:33:34 +1100

    264 - CSRF Protection
    Added CSRF Protection to all the relevant views. Also fixed a bug with the order button.

--- CourseWideApp - Online Store/app.js
+++ CourseWideApp - Online Store/app.js
@@ -14,6 +14,8 @@ const errorController = require('./controllers/error');
 // Custom Database Module
 const mongoose = require('mongoose');
 const MongoDBStore = require('connect-mongodb-session')(session);
+// Custom Security Modules
+const csrf = require('csurf');
 // Custom Data Models
 const User = require('./models/user');
 
@@ -25,6 +27,8 @@ const store = new MongoDBStore({
 	uri: dbURI,
 	collection: 'sessions'
 });
+// Here we are initialising our CSRF protection. A CSRF attack is when a user is tricked into performing an action they did not intend to do. This is done by sending a request to the server from a different website. Any time we are dealing with sensitive or consequential actions, we should protect against a potential CSRF attack.
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -39,6 +43,8 @@ app.use(
 		store: store
 	})
 );
+// After we initialise the session, we can add the CSRF protection to the middleware stack. This will add a token to every request that is sent to the server. This token is then checked against the token stored in the session. If the tokens do not match, the request is rejected.
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
 	if (!req.session.user) {
@@ -50,6 +56,15 @@ app.use((req, res, next) => {
 			next();
 		})
 		.catch(err => console.log(err));
+});
+
+// IMPORTANT - CSURF is a deprecated package, and real world usages should be replaced with a new package.
+// Here we are adding a new middleware that will tell ejs that there is some data we wish to add to every single view. This is the CSRF token. We are also adding a boolean value to the view that will tell us if the user is authenticated or not.
+app.use((req, res, next) => {
+	// The locals field allows us to add local variables that will be passed to every view. This is useful for adding data that is used in every view.
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
 });
 
 app.use('/admin', adminRoutes);

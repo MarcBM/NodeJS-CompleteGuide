18f0d08eea37e23a0c3b57b8fed7a5a68437ad75
Marc Betbeder-Matibet <marc.betbeder@hotmail.com>
2024-02-19 12:53:56 +1100

    Cleanup from last module

--- CourseWideApp - Online Store/controllers/auth.js
+++ CourseWideApp - Online Store/controllers/auth.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-	// To access our flash message, we simply call the flash method, only providing the key. This will remove the message from the session.
-	// The flash method will return an empty array, not null, if no flash messages exist with the given key, so here we do some simple logic to extract our actual message.
 	let message = req.flash('error');
 	if (message.length > 0) {
 		message = message[0];
@@ -32,24 +30,17 @@ exports.getSignup = (req, res, next) => {
 	});
 };
 
-// Instead of just finding our dummy user and creating a session, we are going to find the user based on their provided information.
 exports.postLogin = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
 	User.findOne({ email: email })
 		.then(user => {
 			if (!user) {
-				// If no such user exists based on the email provided, we return to the login page. We should post an error to the user here telling them why they didn't manage to log in.
-				// To do this, we are going to "flash" a message onto the session to be displayed on the Login Screen as an error. This means we are adding a message to the session that will be removed after being shown to the user.
-				// The flash() method takes a key and a value. The key is the type of message we are adding. The value is the message itself.
 				req.flash('error', 'Invalid email or password.');
 				return res.redirect('/login');
 			}
-			// If we made it passed here, we know the email exists, but we still need to check the password.
-			// We can use bcrypt to re-hash the password and compare it to the hashed password in the database.
 			bcrypt
 				.compare(password, user.password)
-				// bcrypt.compare will not throw an error if the passwords do not match. Instead, it will return a boolean value. We can use this to determine if the passwords match or not.
 				.then(doMatch => {
 					if (doMatch) {
 						req.session.isLoggedIn = true;
@@ -72,18 +63,14 @@ exports.postLogin = (req, res, next) => {
 		.catch(err => console.log(err));
 };
 
-// Here we are collecting the information from the form and creating a new user. We are then redirecting to the login page.
 exports.postSignup = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
 	const confirmPassword = req.body.confirmPassword;
-	// Normally we would validate data here, but we are skipping that for now. There will be a whole module on this later.
 
-	// If a user with this email already exists, we don't want to make a new user.
 	User.findOne({ email: email })
 		.then(userDoc => {
 			if (userDoc) {
-				// If the user exists, we are redirecting back to the sign-up page. We should post an error to the user here telling them why they didn't manage to sign-up.
 				req.flash(
 					'error',
 					'E-Mail exists already, please pick a different one.'
--- CourseWideApp - Online Store/routes/admin.js
+++ CourseWideApp - Online Store/routes/admin.js
@@ -6,15 +6,10 @@ const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
-
-// /admin/add-product => GET
-// The route is passed through each middleware function provided as arguments in order from left to right. This allows us to check if the user is logged in or not easily before even reaching the route handler.
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
-// /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
 
-// /admin/add-product => POST
 router.post('/add-product', isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
